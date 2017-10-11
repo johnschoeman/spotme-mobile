b57d8df37f24fc6b14f9b25a6a9c0e5c1ea8088a
drstonebraker <drew.stonebraker@gmail.com>
2017-10-10 19:23:58 -0700

    Get post session working

--- src/components/screens/SessionForm/SessionForm.js
+++ src/components/screens/SessionForm/SessionForm.js
@@ -1,121 +1,120 @@
 import React, { Component } from 'react';
 // import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants';
 import { gql, graphql, compose } from 'react-apollo'
-import { View, Button } from 'react-native'
+import { AsyncStorage, View, Button } from 'react-native'
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 import { NavigationActions } from 'react-navigation'
 
 import styles from '../../../styles/styles'
 
+const GC_USER_ID = 'graphcool-user-id'
+const GC_AUTH_TOKEN = 'graphcool-auth-token'
 
-export default class SessionForm extends Component {
+
+class SessionForm extends Component {
 
   state = {
-    login: true, // switch between Login and SignUp
     email: '',
     password: '',
-    name: ''
   };
 
   render() {
-    const resetNavigateHome = NavigationActions.reset({
-      index: 0,
-      actions: [
-        NavigationActions.navigate({ routeName: 'Home' })
-      ]
-    })
-    const { dispatch } = this.props.navigation;
+
 
     return (
       <View style={styles.screen}>
-        <FormLabel>Username</FormLabel>
-        <FormInput onChangeText={(username) => this.setState({username})}/>
+        <FormLabel>Email</FormLabel>
+        <FormInput onChangeText={(email) => this.setState({email})}/>
         <FormValidationMessage>Error message</FormValidationMessage>
         <FormLabel>Password</FormLabel>
         <FormInput onChangeText={(password) => this.setState({password})}/>
         <FormValidationMessage>Error message</FormValidationMessage>
-        {/* TODO Button onPress should be a function mapped from a thunk action creator */}
+
         <Button
-          onPress={() => dispatch(resetNavigateHome)}
+          onPress={() => this._confirm()}
           title='Submit' />
       </View>
     )
   }
-//
-//   _confirm = async () => {
-//     const { name, email, password } = this.state
-//     if (this.state.login) {
-//       const result = await this.props.signinUserMutation({
-//         variables: {
-//           email,
-//           password
-//         }
-//       })
-//       const id = result.data.signinUser.user.id
-//       const token = result.data.signinUser.token
-//       this._saveUserData(id, token)
-//     } else {
-//       const result = await this.props.createUserMutation({
-//         variables: {
-//           name,
-//           email,
-//           password
-//         }
-//       })
-//       const id = result.data.signinUser.user.id
-//       const token = result.data.signinUser.token
-//       this._saveUserData(id, token)
-//     }
-//     this.props.history.push(`/`)
-//   }
-//
-//   _saveUserData = (id, token) => {
-//     localStorage.setItem(GC_USER_ID, id)
-//     localStorage.setItem(GC_AUTH_TOKEN, token)
-//   }
-//
+
+  _confirm = async () => {
+    const { email, password } = this.state
+    if (this.props.navigation.state.params.formType === 'logIn') {
+      const result = await this.props.signinUserMutation({
+        variables: {
+          email,
+          password
+        }
+      })
+      const id = result.data.signinUser.user.id
+      const token = result.data.signinUser.token
+      this._saveUserData(id, token)
+    } else {
+      const result = await this.props.createUserMutation({
+        variables: {
+          email,
+          password
+        }
+      })
+      const id = result.data.signinUser.user.id
+      const token = result.data.signinUser.token
+
+      this._saveUserData(id, token)
+      const resetNavigateHome = NavigationActions.reset({
+        index: 0,
+        actions: [ NavigationActions.navigate({ routeName: 'Home' }) ]
+      })
+      const { dispatch } = this.props.navigation;
+      dispatch(resetNavigateHome)
+    }
+  }
+
+  _saveUserData = (id, token) => {
+    AsyncStorage.setItem(GC_USER_ID, id)
+    AsyncStorage.setItem(GC_AUTH_TOKEN, token)
+  }
+
 }
-//
-// const CREATE_USER_MUTATION = gql`
-// mutation CreateUserMutation($name: String!, $email: String!, $password: String!) {
-//   createUser(
-//     name: $name,
-//     authProvider: {
-//       email: {
-//         email: $email,
-//         password: $password
-//       }
-//     }
-//   ) {
-//     id
-//   }
-//   signinUser(email: {
-//     email: $email,
-//     password: $password
-//   }) {
-//     token
-//     user {
-//       id
-//     }
-//   }
-// }
-// `
-//
-// const SIGNIN_USER_MUTATION = gql`
-// mutation SigninUserMutation($email: String!, $password: String!) {
-//   signinUser(email: {
-//     email: $email,
-//     password: $password
-//   }) {
-//     token
-//     user {
-//       id
-//     }
-//   }
-// }
-// `
-//
-// export default compose(
-// graphql(CREATE_USER_MUTATION, { name: 'createUserMutation' }),
-// graphql(SIGNIN_USER_MUTATION, { name: 'signinUserMutation' })
-// )(Login);
+
+const CREATE_USER_MUTATION = gql`
+mutation CreateUserMutation($email: String!, $password: String!) {
+  createUser(
+    authProvider: {
+      email: {
+        email: $email,
+        password: $password
+      }
+    }
+  ) {
+    id
+  }
+  signinUser(email: {
+    email: $email,
+    password: $password
+  }) {
+    token
+    user {
+      id
+    }
+  }
+}
+`
+
+const SIGNIN_USER_MUTATION = gql`
+mutation SigninUserMutation($email: String!, $password: String!) {
+  signinUser(email: {
+    email: $email,
+    password: $password
+  }) {
+    token
+    user {
+      id
+    }
+  }
+}
+`
+
+export default compose(
+graphql(CREATE_USER_MUTATION, { name: 'createUserMutation' }),
+graphql(SIGNIN_USER_MUTATION, { name: 'signinUserMutation' })
+)(SessionForm);
