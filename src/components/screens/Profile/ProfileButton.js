b0a6fb868da226abbafebb36be101c67c342f8d0
drstonebraker <drew.stonebraker@gmail.com>
2017-10-11 11:50:04 -0700

    Factor out profile button

--- src/components/screens/Profile/ProfileButton.js
+++ src/components/screens/Profile/ProfileButton.js
@@ -2,42 +2,37 @@ import React from 'react'
 import { View, Text, TouchableHighlight, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/Entypo';
 
-import ProfileButton from './ProfileButton'
-
-export default class ProfileScreen extends React.Component {
+export default class ProfileButton extends React.Component {
 
   // constructor(props) {
   //   super(props)
   // }
 
   render() {
-    const { navigate } = this.props.navigation
+    const { title, description, onPress } = this.props
 
     return (
-      <View style={localStyles.screen}>
-        <Text style={localStyles.header}>PROFILE</Text>
-        <ProfileButton
-          title='Personal Information'
-          description='Edit your profile.'
-          onPress={() => navigate('')}/>
-      </View>
+
+      <TouchableHighlight onPress={onPress}>
+        <View style={localStyles.profileButton}>
+          <View>
+            <Text style={localStyles.profileButtonTitle}>
+              { title }
+            </Text>
+            <Text style={localStyles.profileButtonDescription}>
+              { description }
+            </Text>
+          </View>
+          <Icon name='chevron-thin-right' size={30}/>
+        </View>
+      </TouchableHighlight>
+
     )
   }
 
 }
 
 const localStyles = StyleSheet.create({
-  screen: {
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-
-  },
-  header: {
-    fontSize: 50,
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-  },
   profileButton: {
     borderWidth: 1,
     borderColor: '#000',
