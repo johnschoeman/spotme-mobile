8fc6084db897137101283fca4788d801e7c6983d
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-10-12 09:55:52 -0700

    Changed yarn

--- src/components/screens/Home/Components/SpotPreview.js
+++ src/components/screens/Home/Components/SpotPreview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 import Modal from 'react-native-modal';
 import { Animated, LayoutAnimation } from 'react-native';
@@ -10,31 +10,33 @@ export default class SpotPreview extends React.Component {
 		super()
 		this._renderPreview = this._renderPreview.bind(this)
 		this.state = {
-			marker: null,
-			height: 0
+			marker: null
 		}
 	}
 
 	componentWillReceiveProps(newProps){
 		if (this.state.marker !== newProps.marker) {
-			this.setState({marker: newProps.marker})
+			console.log("I HAVE RECEIVED NEW PROPS", newProps)
+			this.setState({marker: newProps.activeMarker})
 		}
 	}
 
 	_renderPreview(){
+    const { height, width } = Dimensions.get('window')
 		if (this.state.marker) {
 			return(
-				<View> 
+				<Animated.View style={{height: 150, width}}> 
 					<Text>{this.state.marker.title}</Text>
-				</View> 
+				</Animated.View> 
 			)
 		}
 	}
 
 	render(){
-		console.log(this.state.marker);
+    console.log(this.state.marker);
+    
 		return(
-			<View style={{height: 150, width: 150}}>
+      <View style={{position: "absolute", bottom: 0, backgroundColor: "green", zIndex: 9999 }}>
 				{this._renderPreview()}
 			</View>
 		)
