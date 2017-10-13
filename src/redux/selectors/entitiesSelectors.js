69fc0cccccc8af64543d34d5ecab4f7ccb838720
drstonebraker <drew.stonebraker@gmail.com>
2017-10-12 21:53:52 -0700

    Remove todo

--- src/redux/selectors/entitiesSelectors.js
+++ src/redux/selectors/entitiesSelectors.js
@@ -2,7 +2,6 @@ export const getOwnSpots = (state) => {
   const allSpots = Object.values(state.entities.spots)
   const currentUser = state.session.currentUser
 
-  // TODO: change back to currentUser.id
   const ownSpots = allSpots.filter(spot => spot.host_id === currentUser.id)
   return ownSpots
 }
