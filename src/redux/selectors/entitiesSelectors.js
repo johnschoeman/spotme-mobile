f0343054abf7914dbce29082927e569baecd7e76
drstonebraker <drew.stonebraker@gmail.com>
2017-10-12 21:36:59 -0700

    Change entitiesSelector while current user is always first on backend (temporary workaround)

--- src/redux/selectors/entitiesSelectors.js
+++ src/redux/selectors/entitiesSelectors.js
@@ -2,6 +2,7 @@ export const getOwnSpots = (state) => {
   const allSpots = Object.values(state.entities.spots)
   const currentUser = state.session.currentUser
 
-  const ownSpots = allSpots.filter(spot => spot.hostId === currentUser.id)
+  // TODO: change back to currentUser.id
+  const ownSpots = allSpots.filter(spot => spot.hostId === 1)//currentUser.id)
   return ownSpots
 }
