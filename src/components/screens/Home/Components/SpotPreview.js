e4ae980558d9142e7f03642c13bff166e627f448
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-10-12 18:29:47 -0700

    Add content to full spot view

--- src/components/screens/Home/Components/SpotPreview.js
+++ src/components/screens/Home/Components/SpotPreview.js
@@ -46,13 +46,38 @@ export default class SpotPreview extends React.Component {
 	_renderFull(){
 		const { height, width } = Dimensions.get('window')
 		if (this.state.marker) {
+			console.log("MARKER SETTINGS!!!!!", this.state.marker)
 			return (
 				<Animated.View style={{ height: this.state.height, width, flexDirection: "column", paddingTop: 10 }}>
-					<View style={{ justifyContent: 'center' }}>
+					<View style={{ flexDirection: 'row', paddingTop: 10, paddingRight: 5, paddingLeft: 5, justifyContent: 'space-between' }}>
+						<View style={{
+							flexDirection: 'column'}}>
+							<Text>825 Tehama st,</Text>
+							<Text>San Francisco, CA</Text>
+						</View>
+						<View style={{ flexDirection: 'column' }}>
+							<StarRating
+								disabled={false}
+								maxStars={5}
+								rating={this.state.marker.rating}
+								selectedStar={(rating) => {
+									let marker = this.state.marker
+									marker.rating = rating
+									this.setState({ marker: marker })
+								}}
+								starSize={20}
+							/>
+							<Text style={{ fontSize: 18 }}>${this.state.marker.price}.00/hr</Text>
+						</View>
+					</View>
+					<View style={{ justifyContent: 'center', paddingTop: 10 }}>
 						<Image
 							style={{ height: 200, width: width }}
 							source={{ uri: 'http://res.cloudinary.com/ddgt25kwb/image/upload/v1507653351/garage-spot_bcnnyu.jpg' }}
 						/>
+					</View>
+					<View style={{ justifyContent: 'center', paddingTop: 10 }}>
+						<Text>RESERVE NOW</Text>
 					</View>
 				</Animated.View>
 			)
