3a6464d74cb7784f7f30704dc8be8264e293ddd4
drstonebraker <drew.stonebraker@gmail.com>
2017-10-13 11:14:26 -0700

    Revert temporary testing changes

--- src/components/screens/HomeConditional/HomeConditionalScreen.js
+++ src/components/screens/HomeConditional/HomeConditionalScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AsyncStorage, Button } from 'react-native'
+import { AsyncStorage } from 'react-native'
 
 import HomeScreen from '../Home/HomeScreen'
 import WelcomeScreen from '../Welcome/WelcomeScreen'
@@ -27,17 +27,58 @@ export default class HomeConditionalScreen extends React.Component {
     const { isLoggedIn, navigation } = this.props
     const { navigate } = navigation
     return (
-        
+
       isLoggedIn ? (
-        <Button
-        onPress={() => navigate('HostSpotIndex')}
-        title='spot index' />
+        <HomeScreen navigation={navigation} />
       ) : (
         <WelcomeScreen navigation={navigation} />
       )
     )
   }
-  
+
 }
 
-/* <HomeScreen navigation={navigation} /> */
+// TODO: delete this
+// use this version to test spot creation
+
+// import React from 'react'
+// import { AsyncStorage, Button } from 'react-native'
+//
+// import HomeScreen from '../Home/HomeScreen'
+// import WelcomeScreen from '../Welcome/WelcomeScreen'
+//
+//
+// export default class HomeConditionalScreen extends React.Component {
+//
+//   static navigationOptions = (props) => {
+//     // const { isLoggedIn } = props
+//     // need to redo if we are keeping the title bar.
+//     // navigationOptions does not have access to all of props,
+//     // so isLoggedIn is always considered undefined here
+//     // more info: https://reactnavigation.org/docs/navigators/navigation-options
+//     return {
+//       // title: isLoggedIn ? 'Home' : 'Welcome',
+//       title: 'Home'
+//     }
+//   }
+//
+//   constructor(props) {
+//     super(props)
+//   }
+//
+//   render() {
+//     const { isLoggedIn, navigation } = this.props
+//     const { navigate } = navigation
+//     return (
+//
+//       isLoggedIn ? (
+//         <Button
+//         onPress={() => navigate('HostSpotIndex')}
+//         title='spot index' />
+//       ) : (
+//         <WelcomeScreen navigation={navigation} />
+//       )
+//     )
+//   }
+//
+// }
