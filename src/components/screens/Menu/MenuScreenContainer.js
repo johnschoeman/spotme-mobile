8912f3978435c3a753a11f77b15224000926dda7
drstonebraker <drew.stonebraker@gmail.com>
2017-10-13 19:25:05 -0700

    Build menu page

---
+++ src/components/screens/Menu/MenuScreenContainer.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'
+
+import MenuScreen from './MenuScreen'
+
+const mapStateToProps = (state, ownProps) => {
+
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+  return {
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MenuScreen);
