107f981a87ac9a66feaf89b522c637aeff0b6527
drstonebraker <drew.stonebraker@gmail.com>
2017-10-15 00:06:15 -0700

    Add more to reservation form

--- src/components/screens/ReservationForm/ReservationForm.js
+++ src/components/screens/ReservationForm/ReservationForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo'
-import { View } from 'react-native'
+import { View, Slider, Text, StyleSheet } from 'react-native'
+import { FormLabel } from 'react-native-elements'
 import { NavigationActions } from 'react-navigation'
-import Slider from "react-native-slider";
 
 import { CREATE_RESERVATION_MUTATION } from
     '../../../graphql/mutations/ReservationMutations'
@@ -10,40 +10,62 @@ import styles from '../../../styles/styles'
 
 class ReservationForm extends Component {
 
-    constructor(props) {
-      super(props)
+  constructor(props) {
+    super(props)
 
-      this.state = {
-        end_time: new Date(),
-      };
-    }
+    this.state = {
+      duration: 60,
+    };
+  }
 
-    _handleSubmit = async () => {
-        const { spot_id, start_time, end_time } = this.state
-        const reservationVariables = { variables: { spot_id, start_time, end_time } }
-        const result = await this.props.createReservationMutation(reservationVariables)
-        this._navigateHome()
-    }
+  addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+  }
 
-    _navigateHome() {
-        const resetNavigateHome = NavigationActions.reset({
-            index: 0,
-            actions: [NavigationActions.navigate({ routeName: 'Home' })]
-        })
-        const { dispatch } = this.props.navigation;
-        dispatch(resetNavigateHome)
-    }
+  _handleSubmit = async () => {
+      const { duration } = this.state
+      const { spot_id } = this.props.spot
+      const now = new Date()
+      const start_time = now.toISOString()
+      const end_time = this.addMinutes(now, duration).toISOString()
+      const reservationVariables = { variables: { spot_id, start_time, end_time } }
+      const result = await this.props.createReservationMutation(reservationVariables)
+      // resetNavigateHome()
+  }
 
-    render() {
+  timeUnitsToString(time) {
+    timeString = String(time)
+    return time < 10 ? `0${timeString}` : timeString
+  }
 
-        return (
-          <View style={styles.screen}>
-            <Slider />
-          </View>
-        )
-    }
+  render() {
+    const { duration } = this.state
+    const hours = Math.floor( duration / 60)
+    const hoursString = this.timeUnitsToString(hours)
+    const minutes = duration % 60
+    const minutesString = this.timeUnitsToString(minutes)
+    const now = new Date()
+    const nowString = now.toLocaleTimeString()
 
 
+    return (
+      <View style={styles.screen}>
+        <Text>Reserve this spot!</Text>
+        <FormLabel>How long?</FormLabel>
+        <View>
+          <Text>Duration: {`${hoursString}:${minutesString}`}</Text>
+          <Text>Ending: {timeNow}</Text>
+        </View>
+        <Slider
+          maximumValue={60 * 24}
+          minimumValue={15}
+          step={15}
+          value={this.state.duration}
+          onValueChange={(val) => this.setState({duration: val})}
+          />
+      </View>
+    )
+  }
 }
 
 export default compose(
