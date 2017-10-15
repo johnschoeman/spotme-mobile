c85008a2707778b99111a7d99dddeb6ae7884902
Eric Tung <eric.p.tung@gmail.com>
2017-10-14 19:41:35 -0700

    Add swipe down feature to full-page

--- src/components/screens/SpotShow/SpotShow.js
+++ src/components/screens/SpotShow/SpotShow.js
@@ -8,16 +8,36 @@ export default class SpotShow extends React.Component {
   constructor() {
     super()
     this.state = {
-      spot: {}
+      spot: {},
+      height: 0
     }
   }
 
   componentDidMount() {
-    this.setState({ spot: this.props.spot});
+    const { height } = Dimensions.get('window')
+    this.setState({ spot: this.props.spot,
+                    height });
+  }
+
+  onSwipe(gestureName, gestureState) {
+    const { height, width } = Dimensions.get('window')
+    const { SWIPE_DOWN } = swipeDirections;
+    switch (gestureName) {
+      case SWIPE_DOWN:
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        this.setState({ height: 0 })
+        break;
+      default:
+        return
+    }
   }
 
   render() {
     const { height, width } = Dimensions.get('window')
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 10,
+    };
     if (this.state.spot) {
       const spot = this.state.spot
       const address1 = spot.address_street && spot.address_number ? 
@@ -29,41 +49,47 @@ export default class SpotShow extends React.Component {
       const description = spot.description ? spot.description : "N/A"
       return (
         <Animated.View style={{ height: this.state.height, width, flexDirection: "column", paddingTop: 10 }}>
-          <View style={{ flexDirection: 'row', paddingTop: 10, paddingRight: 5, paddingLeft: 5, justifyContent: 'space-between' }}>
-            <View style={{
-              flexDirection: 'column'
-            }}>
-              <Text>{address1}</Text>
-              <Text>{address2}</Text>
+          <GestureRecognizer
+            onSwipe={(direction, state) => this.onSwipe(direction, state)}
+            onPress={() => this.setState({ height: height - 25 })}
+            config={config}
+          >
+            <View style={{ flexDirection: 'row', paddingTop: 10, paddingRight: 5, paddingLeft: 5, justifyContent: 'space-between' }}>
+              <View style={{
+                flexDirection: 'column'
+              }}>
+                <Text>{address1}</Text>
+                <Text>{address2}</Text>
+              </View>
+              <View style={{ flexDirection: 'column' }}>
+                <StarRating
+                  disabled={true}
+                  maxStars={5}
+                  rating={this.state.spot.rating}
+                  selectedStar={(rating) => {
+                    let spot = this.state.spot
+                    spot.rating = rating
+                    this.setState({ spot: spot })
+                  }}
+                  starSize={20}
+                />
+                <Text style={{ fontSize: 18 }}>${this.state.spot.price}.00/hr</Text>
+              </View>
             </View>
-            <View style={{ flexDirection: 'column' }}>
-              <StarRating
-                disabled={false}
-                maxStars={5}
-                rating={this.state.spot.rating}
-                selectedStar={(rating) => {
-                  let spot = this.state.spot
-                  spot.rating = rating
-                  this.setState({ spot: spot })
-                }}
-                starSize={20}
+            <View style={{ justifyContent: 'center', paddingTop: 10 }}>
+              <Image
+                style={{ height: 200, width: width }}
+                source={{ uri: 'http://res.cloudinary.com/ddgt25kwb/image/upload/v1507653351/garage-spot_bcnnyu.jpg' }}
               />
-              <Text style={{ fontSize: 18 }}>${this.state.spot.price}.00/hr</Text>
             </View>
-          </View>
-          <View style={{ justifyContent: 'center', paddingTop: 10 }}>
-            <Image
-              style={{ height: 200, width: width }}
-              source={{ uri: 'http://res.cloudinary.com/ddgt25kwb/image/upload/v1507653351/garage-spot_bcnnyu.jpg' }}
-            />
-          </View>
-          <View>
-            <Text>DESCRIPTION: </Text>
-            <Text>{description}</Text>
-          </View>
-          <View style={{ justifyContent: 'center', paddingTop: 10 }}>
-            <Text>RESERVE NOW: </Text>
-          </View>
+            <View>
+              <Text>DESCRIPTION: </Text>
+              <Text>{description}</Text>
+            </View>
+            <View style={{ justifyContent: 'center', paddingTop: 10 }}>
+              <Text>RESERVE NOW: </Text>
+            </View>
+          </GestureRecognizer>
         </Animated.View>
       )
     }
