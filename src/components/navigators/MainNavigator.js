72e3cf7a1bb7bc10ab493de05d980dd49199005e
John Schoeman <johnschoeman1617@gmail.com>
2017-10-14 21:25:34 -0700

    Fix fb login form

--- src/components/navigators/MainNavigator.js
+++ src/components/navigators/MainNavigator.js
@@ -9,24 +9,18 @@ import HostSpotIndexScreenContainer from
   '../screens/HostSpotIndex/HostSpotIndexScreenContainer'
 import NewSpotScreenContainer from
   '../screens/NewSpot/NewSpotScreenContainer'
-<<<<<<< HEAD:src/components/navigators/MainNavigator.js
 import MenuScreenContainer from
   '../screens/Menu/MenuScreenContainer'
 
-=======
 import SpotShowScreen from '../screens/SpotShow/SpotShowScreen'
->>>>>>> a8202bb6aac8373f96c9912c0c9e8e444e12206d:src/components/navigators/Main.js
 
 const MainNavigator = StackNavigator({
   Home: { screen: HomeConditionalScreenContainer },
   SessionForm: { screen: SessionFormScreen },
   HostSpotIndex: { screen: HostSpotIndexScreenContainer },
   NewSpot: { screen: NewSpotScreenContainer },
-<<<<<<< HEAD:src/components/navigators/MainNavigator.js
   Menu: { screen: MenuScreenContainer },
-=======
   SpotShow: { screen: SpotShowScreen }
->>>>>>> a8202bb6aac8373f96c9912c0c9e8e444e12206d:src/components/navigators/Main.js
   // Profile: { screen: ProfileScreen },
 })
 
