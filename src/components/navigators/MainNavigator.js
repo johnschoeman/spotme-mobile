8020fc97800e813e74fddbb043f3c52de3eb5b3c
drstonebraker <drew.stonebraker@gmail.com>
2017-10-15 18:54:35 -0700

    Conditionally render host spot show

--- src/components/navigators/MainNavigator.js
+++ src/components/navigators/MainNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Platform} from 'react-native'
 import { StackNavigator } from 'react-navigation';
 import HomeConditionalScreenContainer from
   '../screens/HomeConditional/HomeConditionalScreenContainer'
@@ -10,20 +9,15 @@ import NewSpotScreenContainer from
   '../screens/NewSpot/NewSpotScreenContainer'
 import MenuScreenContainer from
   '../screens/Menu/MenuScreenContainer'
-import SpotShowContainer from '../screens/SpotShow/SpotShowContainer'
+import SpotShowScreen from '../screens/SpotShow/SpotShowScreen'
 
 const MainNavigator = StackNavigator({
   Home: { screen: HomeConditionalScreenContainer },
   HostSpotIndex: { screen: HostSpotIndexScreenContainer },
   NewSpot: { screen: NewSpotScreenContainer },
   Menu: { screen: MenuScreenContainer },
-  SpotShow: { screen: SpotShowContainer },
+  SpotShow: { screen: SpotShowScreen },
   // Profile: { screen: ProfileScreen },
-}, {
-  navigationOptions: {
-    headerTitleStyle: { color: 'white' },
-    headerStyle: Platform.OS === 'ios' ? { backgroundColor: 'rgb(150, 0, 0)' } : { backgroundColor: 'rgb(150, 0, 0)', marginTop: 25 }
-  }
 })
 
 export default MainNavigator;
