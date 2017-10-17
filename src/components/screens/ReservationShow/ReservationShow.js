330d893313844d4b3f0e57eb4c846031e89368e0
Eric Tung <eric.p.tung@gmail.com>
2017-10-17 11:18:33 -0700

    Fix bug where timer initialized incorrectly

--- src/components/screens/ReservationShow/ReservationShow.js
+++ src/components/screens/ReservationShow/ReservationShow.js
@@ -13,7 +13,7 @@ export default class ReservationShow extends React.Component {
   componentDidMount() {
     const endTime = this.props.navigation.state.params.reservation.createReservation.end_time_since_epoch
     const startTime = new Date().getTime()
-    this.setState({ timeRemaining: endTime - startTime })
+    this.setState({ timeRemaining: Math.floor((endTime - startTime) / 1000)})
     Timer.setInterval(this, 'timer', 
                       () => {
                         if(this.state.timeRemaining > 0) {
@@ -36,7 +36,6 @@ export default class ReservationShow extends React.Component {
     const timeRemaining = this.state.timeRemaining
     const timeRemainingHours = this.pad(Math.floor((timeRemaining/3600)))
     const timeRemainingMinutes = this.pad(Math.floor(((timeRemaining % 3600) /60)))
-    console.log("Time remaining minutes", timeRemainingMinutes)
     const timeRemainingSeconds = this.pad(timeRemaining % 60)
     const timeRemainingString = `${timeRemainingHours}:${timeRemainingMinutes}:${timeRemainingSeconds}`
     return (
